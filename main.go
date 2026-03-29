81b712bc401d7c3d93f57a4949bb585e1c87883f
erlint1212 <e.t.nordtvedt@gmail.com>
2026-03-29 22:47:03 +0200

    Added go routine

--- main.go
+++ main.go
@@ -2,8 +2,61 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"os"
+	"log"
+	"io"
 )
 
-func main() {
-	fmt.Println("I hope I get the job!")
+const inputFilePath = "messages.txt"
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	buffer := make([]byte, 8)
+	currentLine := ""
+
+	messages := make(chan string)
+
+	go func () {
+		for {
+			n, err := file.Read(buffer)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				log.Fatalf("ERROR: Couldn't read bytes from file: %v", err)
+			}
+
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			if len(parts) == 1 {
+				currentLine += parts[0]
+			} else {
+				for i:=0;i < len(parts) - 1;i++ {
+					currentLine += parts[i]
+					messages <- currentLine
+				}
+				currentLine = "" + parts[len(parts)-1]
+			}
+		}
+		if currentLine != "" {
+			messages <- currentLine
+		}
+		close(messages)
+		file.Close()
+	} ()
+
+	return messages
 }
+
+func main() {
+	file, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("ERROR: Couldn't open file: %v\n", err)
+	}
+
+
+	for msg := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", msg)
+	}
+}
+
