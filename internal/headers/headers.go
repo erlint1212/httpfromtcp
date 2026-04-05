288f33df9e7b229c7db576efb484846aca7b3489
erlint1212 <e.t.nordtvedt@gmail.com>
2026-04-06 01:23:08 +0200

    Minor changes to move from snake case to camelCase

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -8,33 +8,32 @@ import (
 type Headers map[string]string
 
 const CRLF = "\r\n"
-const bufferSize = 8
 
 func NewHeaders() Headers {
 	return Headers{}
 }
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
-	data_string := string(data)
+	dataString := string(data)
 
-	data_string_split := strings.Split(data_string, CRLF)
-	if len(data_string_split) == 1 {
+	dataStringSplit := strings.Split(dataString, CRLF)
+	if len(dataStringSplit) == 1 {
 		return 0, false, nil
 	}
-	if data_string_split[0][:len(CRLF)-1] == CRLF {
-		return len(data_string_split[0]) + len(CRLF), true, nil
+	if dataStringSplit[0] == "" {
+		return len(CRLF), true, nil
 
-	} 
-
-	header := data_string_split[0]
-	header_key_value := strings.Split(header, ":")
-	if strings.TrimSpace(header_key_value[0]) != header_key_value[0] {
-		return 0, false, fmt.Errorf("Whitespace found in field-name: %s", header_key_value[0])
 	}
-	header_value := strings.Join(header_key_value[1:], ":")
-	header_value = strings.TrimSpace(header_value)
 
-	h[header_key_value[0]] = header_value
+	header := dataStringSplit[0]
+	headerKeyValue := strings.Split(header, ":")
+	if strings.Contains(headerKeyValue[0], " ") {
+		return 0, false, fmt.Errorf("invalid field-name: %s", headerKeyValue[0])
+	}
+	headerValue := strings.Join(headerKeyValue[1:], ":")
+	headerValue = strings.TrimSpace(headerValue)
 
-	return len(data_string_split[0]) + len(CRLF), false, nil
+	h[headerKeyValue[0]] = headerValue
+
+	return len(dataStringSplit[0]) + len(CRLF), false, nil
 }
